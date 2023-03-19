a786fbf8c58278877a03f9df22b2622560671fe5
JosuaLimbu <osaltusal@gmail.com>
2023-03-19 22:59:34 +0800

    Menambahkan custom link API html

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started with Node JS by Josua Limbu
+
+## Available Scripts
+
+In the project directory, you can run:
+
+## `npm install`
+install all packages from package.json on the cors web server
+
+## `node index.js`
+Runs the app in the development mode.
+
+## Last Step
+
+Run html **index.html** and fill in the web server link you want to pull the API into the html display field
--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const express = require('express');
+const members = require('./members');
+const getUsers = require('./users');
 const cors = require('cors')
 
 const app = express();
-
-app.use(cors())
+app.use(cors());
 const port = 3000;
 
-app.get('/', (req, res) => res.json({data: 'Halo bang'}));
+app.get('/', (req, res) => {
+    res.json({data: 'Halo bang, This is the homepage'})
+});
+
+app.get('/about', (req, res) => {
+    const date = new Date().toISOString();
+    res.json({
+        status: 'success',
+        message: 'response success',
+        description: 'Exercise #03',
+        date: date,
+        data: members
+    })
+});
+app.get('/users', async (req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(await getUsers()));
+});
+app.get("/members/:name", (req, res) => {
+  const nama = req.params.name.toLowerCase();
+  const member = members.find((member) => member.nama.toLowerCase() === nama);
+  if (member) {
+    res.json(member);
+  } else {
+      res.status(404).json({
+        message: "Data tidak ditemukan",
+    });
+  }
+});
+//Middleware log
+const log = (req, res, next) =>{
+  console.log(Date.now()+ " " + req.ip + " " + req.originalUrl);
+  next();
+};
+app.use(log);
+
+// Middleware menangani Routing 404
+const notFound = (req, res, next) => {
+  res.json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+};
+app.use(notFound);
+
 
 app.listen(port, () => console.log(`Server run on http://localhost:${port}`));
